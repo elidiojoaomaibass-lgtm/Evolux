c47f20e2a0a12fa5c58ec59607b64b9faf8ca710
joaom <joaomaibass1@gmail.com>
2026-05-17 01:45:50 +0200

    feat: Pagina de Obrigado apos pagamento confirmado com animacoes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThankYouPage } from './components/ThankYouPage.tsx'
+
+const isThankYouPage = window.location.pathname === '/obrigado';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {isThankYouPage ? <ThankYouPage /> : <App />}
   </StrictMode>,
 )
